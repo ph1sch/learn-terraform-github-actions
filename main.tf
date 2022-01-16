d893f9927c58e6e4ffd90e33e5673cc38d115304
Philipp Schuller <ph1sch@gmx.de>
2022-01-16 18:13:35 +0100

    Updated provider versions

--- main.tf
+++ main.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.26.0"
+      version = "3.72.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "3.0.1"
+      version = "3.1.0"
     }
   }
   required_version = "~> 1.0"
